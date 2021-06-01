4cd8724a49f76a1ff2e7012e4b0b313b68438b0c
fahrizdimasqy <1197050037@student.uinsgd.ac.id>
2021-06-01 23:59:28 +0700

    add project ArrayLooping

--- PrakPBO/src/project1/CastingData.java
+++ PrakPBO/src/project1/CastingData.java
@@ -2,6 +2,7 @@ package project1;
 
 public class CastingData {
 	public static void main(String[] args) {
+//		global variabel
 		byte a = 50;
 		int b = 5;
 		float c = 2;
@@ -11,5 +12,14 @@ public class CastingData {
 		
 		System.out.println(d);
 		System.out.println(e);
+		
+		if(d%2 == 0) {
+//			local variabel
+			String genap = "genap";
+			System.out.println("d adalah bilangan " + genap);
+		} else {
+			String ganjil = "ganjil";
+			System.out.println("d adalah bilangan " + ganjil);
+		}
 	}
 }
